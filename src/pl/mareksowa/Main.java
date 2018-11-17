5435b40b710e798c307794f7fd8925d1793fc588
Marek Sowa <Marek.Sowa@ibm.com>
2018-11-17 12:08:35 +0100

    add test cases

--- src/pl/mareksowa/Main.java
+++ src/pl/mareksowa/Main.java
@@ -1,9 +1,29 @@
 package pl.mareksowa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello calc!");
+        System.out.println("witaj w calc");
+        System.out.println("wybierz 'd' by dodawac lub 'exit' by wyjsc z programu");
+        Scanner sc = new Scanner(System.in);
+        String operator = sc.next();
+        switch (operator){
+            case "d":{
+                System.out.println("dodajesz");
+                break;
+            }
+            case "exit":{
+                System.out.println("dowidzenia");
+                System.exit(0);
+                break;
+            }
+            default:{
+                System.out.println("nierozposnano komendy");
+                break;
+            }
+        }
     }
 }
